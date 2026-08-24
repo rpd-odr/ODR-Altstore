aff4a9dac376b3c456c0a8dcbbf4c63602f40365
rpd-odr <127396462+rpd-odr@users.noreply.github.com>
2026-08-24 13:07:26 +0500

    feat: register decrypt.day adapter

--- providers/adapters/__init__.py
+++ providers/adapters/__init__.py
@@ -1,5 +1,6 @@
 from providers.adapters.base import BaseDecryptAdapter
 from providers.adapters.decryptipa import DecryptIPAAdapter
 from providers.adapters.http_ipa import HttpIPASourceAdapter
+from providers.adapters.decryptday import DecryptDayAdapter
 
-__all__ = ["BaseDecryptAdapter", "DecryptIPAAdapter", "HttpIPASourceAdapter"]
+__all__ = ["BaseDecryptAdapter", "DecryptIPAAdapter", "HttpIPASourceAdapter", "DecryptDayAdapter"]
