45b0dcb7dc7c45dfde170b010dbde70d25322699
rpd-odr <127396462+rpd-odr@users.noreply.github.com>
2026-08-24 13:04:09 +0500

    feat: register generic HTTP IPA adapter

--- providers/adapters/__init__.py
+++ providers/adapters/__init__.py
@@ -1,3 +1,5 @@
 from providers.adapters.base import BaseDecryptAdapter
 from providers.adapters.decryptipa import DecryptIPAAdapter
-__all__ = ["BaseDecryptAdapter", "DecryptIPAAdapter"]
+from providers.adapters.http_ipa import HttpIPASourceAdapter
+
+__all__ = ["BaseDecryptAdapter", "DecryptIPAAdapter", "HttpIPASourceAdapter"]
